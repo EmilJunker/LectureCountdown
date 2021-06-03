5c952e49294ea57707569d3685b3fa622481cdf1
Emil Junker <emil.junker@outlook.com>
2021-06-03 16:30:34 +0200

    ConsoleTestApp add more CLI arguments

--- ConsoleTestApp/Program.cs
+++ ConsoleTestApp/Program.cs
@@ -9,11 +9,19 @@ namespace ConsoleTestApp
         {
             int? arg0 = null;
             int? arg1 = null;
+            int? arg2 = null;
+            int? arg3 = null;
+            int? arg4 = null;
+            int? arg5 = null;
 
             try
             {
                 arg0 = int.Parse(args[0]);
                 arg1 = int.Parse(args[1]);
+                arg2 = int.Parse(args[2]);
+                arg3 = int.Parse(args[3]);
+                arg4 = int.Parse(args[4]);
+                arg5 = int.Parse(args[5]);
             }
             catch (Exception) { }
 
@@ -29,10 +37,33 @@ namespace ConsoleTestApp
                 // have arg0 -> use as length
                 Countdown.TimerSetup(starttime, (int)arg0);
             }
-            else
+            else if (arg2 is null)
             {
                 // have arg0 arg1 -> use as endtime today or tomorrow
                 DateTime endtime = Countdown.DateTimeTodayOrTomorrow((int)arg0, (int)arg1, 0);
+                Countdown.TimerSetup(starttime, endtime);
+            }
+            else if (arg3 is null)
+            {
+                // have arg0 arg1 arg2 -> use as endtime
+                DateTime endtime = Countdown.DateTimeToday((int)arg1, (int)arg2, 0);
+                endtime = endtime.AddDays((int)arg0);
+                Countdown.TimerSetup(starttime, endtime);
+            }
+            else if (arg4 is null)
+            {
+                // have arg0 arg1 arg2 arg3 -> use as starttime + arg3 as length
+                starttime = Countdown.DateTimeToday((int)arg1, (int)arg2, 0);
+                starttime = starttime.AddDays((int)arg0);
+                Countdown.TimerSetup(starttime, (int)arg3);
+            }
+            else if (!(arg5 is null))
+            {
+                // have all -> use as starttime + endtime
+                starttime = Countdown.DateTimeToday((int)arg1, (int)arg2, 0);
+                starttime = starttime.AddDays((int)arg0);
+                DateTime endtime = Countdown.DateTimeToday((int)arg4, (int)arg5, 0);
+                endtime = endtime.AddDays((int)arg3);
                 Countdown.TimerSetup(starttime, endtime);
             }
 
